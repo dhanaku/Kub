e18299c10de258f1557cee4a68ce59c104989fd2
dhanaku <dakubattin@gmail.com>
2025-07-27 12:18:34 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "aks-resource-group"
+  location = "East US"
+}
+Create AKS Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
